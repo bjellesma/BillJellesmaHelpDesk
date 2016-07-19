5f603c1d062b5d9c526766592ef6d2fef6ffa0e7
bjellesma <william.jellesma@gmail.com>
2016-07-19 19:11:13 -0400

    Add navbar drawer

---
+++ js/nav.js
@@ -0,0 +1,19 @@
+$( document ).ready(function(){
+  $( ".cross" ).hide();
+  $( "#wj_nav" ).hide();
+  $( ".hamburger" ).click(function() {
+    console.log("hamburger clicked");
+    $( "#wj_nav" ).slideToggle( "slow", function() {
+    $( ".hamburger" ).hide();
+      $( ".cross" ).show();
+    });
+  });
+
+  $( ".cross" ).click(function() {
+    $( "#wj_nav" ).slideToggle( "slow", function() {
+      $( ".cross" ).hide();
+      $( ".hamburger" ).show();
+    });
+  });
+
+});
